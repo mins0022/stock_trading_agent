2b9b5fc129634fc54d4cb9c4f3a44b1e64f306d8
leeyouhee <lyh940310@gmail.com>
2020-11-13 17:37:18 +0900

    front_end 201113
    프론트앤드 개발
    #강화학습 결과에 따른 매도 매수 홀딩 박스 출력 못함

--- frontback/StockTradingAgent/board/urls.py
+++ frontback/StockTradingAgent/board/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('',views.board),
-    path('chart/', views.viewChart)
+    path('chart/', views.viewChart_Samsung)
 ]
